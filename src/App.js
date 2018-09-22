ed2489ea7a959779393ed8ab70649f5112cfdaf6
Hirokuni Kawasaki <Hirokuni.Kawasaki@gmail.com>
2018-09-22 12:26:47 +0900

    Add the sample in retejs start guide

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Rete from "rete";
+import ConnectionPlugin from 'rete-connection-plugin';
+import VueRenderPlugin from 'rete-vue-render-plugin';
+
+const numSocket = new Rete.Socket('Number value');
+
+class NumComponent extends Rete.Component {
+  constructor(){
+      super('Number');
+  }
+
+  builder(node) {
+      let out = new Rete.Output('Number',numSocket); 
+
+      node.addOutput(out);
+  }
+
+  worker(node, inputs, outputs){
+      outputs[0] = node.data.num;
+  }
+}
+
+const container = document.querySelector('#rete');
+const editor = new Rete.NodeEditor('demo@0.1.0', container);
+
+editor.use(ConnectionPlugin)
+editor.use(VueRenderPlugin)
+
+const numComponent = new NumComponent();
+editor.register(numComponent);
+
+const engine = new Rete.Engine('demo@0.1.0');
+editor.register(numComponent);
+
+editor.on('process nodecreated noderemoved connectioncreated connectionremoved', async () => {
+    await engine.abort();            
+    await engine.process(editor.toJSON());            
+});
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +52,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="rete" class="node-editor"></div>
       </div>
     );
   }
